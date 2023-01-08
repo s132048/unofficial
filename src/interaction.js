ce9da6c18d96cb64893ce033c73e4cbc106b465d
hyeseobshim <musicsharing@naver.com>
2023-01-08 22:41:21 +0900

    update

--- src/interaction.js
+++ src/interaction.js
@@ -19,10 +19,7 @@ import eye from './eye.glb';
 import logo from './logo.glb';
 import logo1 from './logo1.glb';
 import logo2 from './logo2.glb';
-import logo3 from './logo4.glb';
-// import logo6 from './logo6.glb';
-// import logo7 from './logo5.glb';
-// import logo8 from './logo8.glb';
+import logo3 from './logo3.glb';
 import work from './work.glb';
 
 import tvstudio from './tvstudio.hdr';
@@ -30,7 +27,7 @@ import tvstudio from './tvstudio.hdr';
 export const mainInteraction = () => {
     const canvas = document.querySelector('#three-canvas');
     // antialias 꼭 넣어야 하나?
-    const renderer = new THREE.WebGLRenderer({canvas, alpha: true, antialias: true});
+    const renderer = new THREE.WebGLRenderer({canvas, alpha: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
     renderer.shadowMap.enabled = true;
@@ -38,43 +35,41 @@ export const mainInteraction = () => {
     // gui
     const gui = new dat.GUI();
     let parameters = {
-        camera: isMobile ? 2 : 3,
-        decay: isMobile ? 0.84 : 0.6,
-        angularDecay: isMobile ? 0.6 : 0.8,
-        force: isMobile ? 2000 : 400,
+        camera: isMobile ? 2 : 3.2,
+        decay: isMobile ? 0.84 : 0.7,
+        angularDecay: isMobile ? 0. : 0.7,
+        force: isMobile ? 1800 : 1300,
         torque: 10,
 
         // 조명
         ambientLight: 0.5,
         environmentLight: 1,
+        massAbout: 800,
+        massContact: 200,
+        massWork: 200,
     }
-    gui.add(parameters, 'camera').min(0).max(10).step(0.1);
-    gui.add(parameters, 'decay').min(0).max(1).step(0.01);
-    gui.add(parameters, 'angularDecay').min(0).max(1).step(0.01);
-    gui.add(parameters, 'force').min(0).max(10000).step(1);
-    gui.add(parameters, 'torque').min(0).max(100).step(1);
-
-    gui.add(parameters, 'ambientLight').min(0).max(10).step(0.01);
-    gui.add(parameters, 'environmentLight').min(0).max(10).step(0.01);
+    gui.add(parameters, 'massAbout').min(10).max(1000).step(1);
+    gui.add(parameters, 'massContact').min(10).max(1000).step(1);
+    gui.add(parameters, 'massWork').min(10).max(1000).step(1);
 
     // init
     const scene = new THREE.Scene();
 
     // 카메라
     const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-    // const camera = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2);
     camera.position.y = parameters.camera;
     scene.add(camera);
 
     // 조명
     const ambientLight = new THREE.AmbientLight('white', parameters.ambientLight);
-    // ambientLight.castShadow = true;
     scene.add(ambientLight);
 
     // 배경
+    let sceneLoaded = false;
     new RGBELoader().load(tvstudio, (texture) => {
         texture.mapping = THREE.EquirectangularReflectionMapping;
         scene.environment = texture;
+        sceneLoaded = true;
     });
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
     renderer.toneMappingExposure = parameters.environmentLight;
@@ -101,11 +96,6 @@ export const mainInteraction = () => {
     let objects = {};
 
     const gltfLoader = new GLTFLoader();
-    const dracoLoader = new DRACOLoader()
-    dracoLoader.setDecoderConfig({ type: 'js' });
-    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
-    dracoLoader.preload();
-    gltfLoader.setDRACOLoader(dracoLoader);
 
     // 메인 로고 넣기
     gltfLoader.load(logo, gltf => {
@@ -124,49 +114,45 @@ export const mainInteraction = () => {
         objects.center.offset = new CANNON.Vec3(0, 0,0);
     });
 
-    // let heavyLogos = [];
-    // for (let i = 0; i < 3; i++) {
-    //     gltfLoader.load(logo8, gltf => {
-    //         로고 사이즈 정하기
-            // heavyLogos.push(gltf.scene.children[0]);
-            // if (i === 2) {
-            //     alert('done');
-            // }
-            // const logoScale = isMobile ? 24 : 32;
-            // gltf.scene.children[0].scale.set(logoScale, logoScale, logoScale);
-            // const sphereBody = new CANNON.Sphere(0.001);
-            // const body = new CANNON.Body({mass: 0, position: new CANNON.Vec3(0, 0,0 ), material: defaultMaterial});
-            // body.addShape(sphereBody);
-            // cannonWorld.addBody(body);
-            //
-            // objects.center = {};
-            // objects.center.mesh = gltf.scene.children[0];
-            // scene.add(gltf.scene.children[0]);
-            // objects.center.body = body;
-            // objects.center.offset = new CANNON.Vec3(0, 0,0);
-        // });
-    // }
+    const models = [
+        {name: 'contact', asset: contact},
+        {name: 'about', asset: about},
+        {name: 'work', asset: work},
+        {name: 'logo3', asset: logo3},
+        {name: 'logo2', asset: logo2},
+        {name: 'logo1', asset: logo1},
+    ]
+    const modelLoaded = {};
+    models.forEach(model => {
+        gltfLoader.load(model.asset, gltf => {
+            modelLoaded[model.name] = gltf.scene.children[0];
+        });
+    })
+    let eyeModel;
+    gltfLoader.load(eye, gltf => {
+        modelLoaded['eye'] = gltf.scene.children[0];
+    });
 
     const sequence = [
-        // {menuName: 'eye', asset: eye, scale: 8, floating: true, mass: isMobile ? 200 : 40, forces: [new CANNON.Vec3(Math.random() - 0.5,Math.random() - 0.5,Math.random() - 0.5).scale(1000)]},
-        // {menuName: 'eye2', asset: eye, scale: isMobile ? 12 : 16, floating: true, mass: isMobile ? 200 : 40, forces: [new CANNON.Vec3(Math.random() - 0.5,Math.random() - 0.5,Math.random() - 0.5).scale(1000)]},
-        // {menuName: 'eye3', asset: eye, scale: isMobile ? 16 : 40, floating: true, mass: isMobile ? 200 : 40, forces: [new CANNON.Vec3(Math.random() - 0.5,Math.random() - 0.5,Math.random() - 0.5).scale(1000)]},
-        // {menuName: 'floating1', asset: logo1, scale: isMobile ? 1.5 : 4, floating: true, mass: isMobile ? 200: 40, forces: [new CANNON.Vec3(2,2,-3).scale(200)]},
-        // {menuName: 'floating2', asset: logo2, scale: isMobile ? 4 : 10, floating: true, mass: isMobile ? 200: 40, forces: [new CANNON.Vec3(-3,4,3).scale(160)]},
-        // {menuName: 'floating3', asset: logo3, scale: isMobile ? 1.5 : 4, floating: true, mass: isMobile ? 200: 40, forces: [new CANNON.Vec3(-4,-2,-3).scale(150)]},
-        // mass 최적인가?
-        {menuName: 'logo1', asset: logo1, mass: 1, scale: isMobile ? 8 : 20, positionX: isMobile ? 0.1 : -2, positionY: -0.6, positionZ: isMobile ? -0.8 : -0.9},
-        {menuName: 'logo2', asset: logo1, mass: 1, scale: isMobile ? 8 : 16, positionX: isMobile ? 0.1 : 1.6, positionY: -0.6, positionZ: isMobile ? -0.8 : -0.3},
-        {menuName: 'logo3', asset: logo1, mass: 1, scale: isMobile ? 8 : 16, positionX: isMobile ? 0.1 : 0.1, positionY: -0.8, positionZ: isMobile ? -0.8 : 0.8},
-        {menuName: 'work', asset: work, mass: 10, scale: isMobile ? 8 : 28, positionX: isMobile ? 0.1 : 0.3, positionY: 0.4, positionZ: isMobile ? -0.8 : -0.6},
-        {menuName: 'about', asset: about, mass: 1, scale: isMobile ? 6 : 16, positionX: isMobile ? 0 : -1.4, positionY: 0, positionZ: isMobile ? 0.75 : 0.8},
-        {menuName: 'contact', asset: contact, mass: 1, scale: isMobile ? 5 : 12, positionX: isMobile ? -0 : 1.5, positionY: 0, positionZ: isMobile ? 0 : 1},
+        {menuName: 'eye', scale: isMobile ? 4 : 10, floating: true, mass: isMobile ? 200 : 40, forces: [new CANNON.Vec3(Math.random() - 0.5,Math.random() - 0.5,Math.random() - 0.5).scale(1000)]},
+        {menuName: 'eye2', scale: isMobile ? 6 : 16, floating: true, mass: isMobile ? 200 : 40, forces: [new CANNON.Vec3(Math.random() - 0.5,Math.random() - 0.5,Math.random() - 0.5).scale(1000)]},
+        {menuName: 'eye3', scale: isMobile ? 8 : 40, floating: true, mass: isMobile ? 200 : 40, forces: [new CANNON.Vec3(Math.random() - 0.5,Math.random() - 0.5,Math.random() - 0.5).scale(1000)]},
+        {menuName: 'eye4', scale: isMobile ? 10 : 6, floating: true, mass: isMobile ? 200 : 40, forces: [new CANNON.Vec3(Math.random() - 0.5,Math.random() - 0.5,Math.random() - 0.5).scale(1000)]},
+        {menuName: 'eye5', scale: isMobile ? 12 : 20, floating: true, mass: isMobile ? 200 : 40, forces: [new CANNON.Vec3(Math.random() - 0.5,Math.random() - 0.5,Math.random() - 0.5).scale(1000)]},
+        {menuName: 'eye6', scale: isMobile ? 13 : 26, floating: true, mass: isMobile ? 200 : 40, forces: [new CANNON.Vec3(Math.random() - 0.5,Math.random() - 0.5,Math.random() - 0.5).scale(1000)]},
+        {menuName: 'eye7', scale: isMobile ? 11 : 40, floating: true, mass: isMobile ? 200 : 40, forces: [new CANNON.Vec3(Math.random() - 0.5,Math.random() - 0.5,Math.random() - 0.5).scale(1000)]},
+        {menuName: 'logo1', mass: 1, scale: isMobile ? 15 : 26, positionX: isMobile ? 0.1 : -2, positionY: isMobile ? -0.5 : -0.6, positionZ: isMobile ? -1.1 : -0.9},
+        {menuName: 'logo2', mass: 1, scale: isMobile ? 4 : 8, positionX: isMobile ? -0.2 : 1.6, positionY: isMobile ? -0.4 : -0.6, positionZ: isMobile ? 0 : -0.3},
+        {menuName: 'logo3', mass: 1, scale: isMobile ? 4 : 8, positionX: isMobile ? -0.1 : -1.8, positionY: isMobile ? -0.5 : -0.6, positionZ: isMobile ? 1 : 1},
+        {menuName: 'work', mass: parameters.massWork, scale: isMobile ? 9 : 28, positionX: isMobile ? 0 : 0.3, positionY: 0.4, positionZ: isMobile ? 0 : -0.5},
+        {menuName: 'about', mass: parameters.massAbout, scale: isMobile ? 6 : 16, positionX: isMobile ? 0 : -1.4, positionY: 0, positionZ: isMobile ? -0.7 : 1},
+        {menuName: 'contact', mass: parameters.massContact, scale: isMobile ? 5 : 12, positionX: isMobile ? 0.1 : 1.5, positionY: 0, positionZ: isMobile ? 0.8 : 1.2},
     ];
 
     const clock = new THREE.Clock();
 
     const planeShape = new CANNON.Plane();
-    const yCap = isMobile ? 0.2 : 1;
+    const yCap = isMobile ? 0.6 : 1;
     var planeYmin = new CANNON.Body({mass: 0, material: defaultContactMaterial});
     planeYmin.addShape(planeShape);
     planeYmin.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
@@ -247,12 +233,6 @@ export const mainInteraction = () => {
                 body.applyImpulse(force);
             }
 
-            // if (name.includes('eye') && Math.random() > 0.5) {
-            //     let impulse = new CANNON.Vec3(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
-            //     impulse.scale(100);
-            //     object.body.applyImpulse(impulse);
-            // }
-
             if (['work', 'about', 'contact', 'logo1', 'logo2', 'logo3'].includes(name) && !object.constraint) {
                 object.constraint = new CANNON.PointToPointConstraint(
                     body,
@@ -267,65 +247,39 @@ export const mainInteraction = () => {
 
     let tempModel;
     const loadSequence = (sequence) => {
+        const menuName = sequence[sequence.length - 1].menuName;
+        if (modelLoaded.hasOwnProperty(menuName)) {
+            tempModel = modelLoaded[menuName];
+        } else if (menuName.includes('eye') && modelLoaded.hasOwnProperty('eye')) {
+            tempModel = modelLoaded['eye'].clone();
+        } else {
+            return;
+        }
         const toLoad = sequence.pop();
-        // if (toLoad.heavy) {
-        //     if (heavyLogos.length === 0) {
-        //         sequence.push(toLoad);
-        //     } else {
-        //         tempModel = heavyLogos.pop();
-        //         tempModel.scale.set(toLoad.scale, toLoad.scale, toLoad.scale);
-        //         const { shape, offset, quaternion } = threeToCannon(tempModel, {type: ShapeType.BOX});
-        //         const body = new CANNON.Body({
-        //             mass: toLoad.mass,
-        //             position: new CANNON.Vec3(-offset.x, -offset.y, -offset.z),
-        //             material: defaultMaterial
-        //         });
-        //         body.addShape(shape, offset, quaternion);
-        //         cannonWorld.addBody(body);
-        //
-        //         objects[toLoad.menuName] = {};
-        //         objects[toLoad.menuName].mesh = tempModel;
-        //         objects[toLoad.menuName].mesh.castShadow = true;
-        //         objects[toLoad.menuName].mesh.receiveShadow = true;
-        //         scene.add(tempModel);
-        //         objects[toLoad.menuName].body = body;
-        //         objects[toLoad.menuName].offset = offset;
-        //         objects[toLoad.menuName].forceExpire = 0;
-        //
-        //         if (toLoad.positionX) objects[toLoad.menuName].positionX = toLoad.positionX;
-        //         if (toLoad.positionX) objects[toLoad.menuName].positionY = toLoad.positionY;
-        //         if (toLoad.positionZ) objects[toLoad.menuName].positionZ = toLoad.positionZ;
-        //         if (toLoad.floating) objects[toLoad.menuName].floating = toLoad.floating;
-        //         if (toLoad.forces) objects[toLoad.menuName].forces = toLoad.forces;
-        //     }
-        //     return;
-        // }
-        gltfLoader.load(toLoad.asset, gltf => {
-            gltf.scene.children[0].scale.set(toLoad.scale, toLoad.scale, toLoad.scale);
-            const { shape, offset, quaternion } = threeToCannon(gltf.scene.children[0], {type: ShapeType.BOX});
-            const body = new CANNON.Body({
-                mass: toLoad.mass,
-                position: new CANNON.Vec3(-offset.x, -offset.y, -offset.z),
-                material: defaultMaterial
-            });
-            body.addShape(shape, offset, quaternion);
-            cannonWorld.addBody(body);
-
-            objects[toLoad.menuName] = {};
-            objects[toLoad.menuName].mesh = gltf.scene.children[0];
-            objects[toLoad.menuName].mesh.castShadow = true;
-            objects[toLoad.menuName].mesh.receiveShadow = true;
-            scene.add(gltf.scene.children[0]);
-            objects[toLoad.menuName].body = body;
-            objects[toLoad.menuName].offset = offset;
-            objects[toLoad.menuName].forceExpire = 0;
-
-            if (toLoad.positionX) objects[toLoad.menuName].positionX = toLoad.positionX;
-            if (toLoad.positionX) objects[toLoad.menuName].positionY = toLoad.positionY;
-            if (toLoad.positionZ) objects[toLoad.menuName].positionZ = toLoad.positionZ;
-            if (toLoad.floating) objects[toLoad.menuName].floating = toLoad.floating;
-            if (toLoad.forces) objects[toLoad.menuName].forces = toLoad.forces;
+        tempModel.scale.set(toLoad.scale, toLoad.scale, toLoad.scale);
+        const { shape, offset, quaternion } = threeToCannon(tempModel, {type: ShapeType.BOX});
+        const body = new CANNON.Body({
+            mass: toLoad.mass,
+            position: new CANNON.Vec3(-offset.x, -offset.y, -offset.z),
+            material: defaultMaterial
         });
+        body.addShape(shape, offset, quaternion);
+        cannonWorld.addBody(body);
+
+        objects[toLoad.menuName] = {};
+        objects[toLoad.menuName].mesh = tempModel;
+        objects[toLoad.menuName].mesh.castShadow = true;
+        objects[toLoad.menuName].mesh.receiveShadow = true;
+        scene.add(tempModel);
+        objects[toLoad.menuName].body = body;
+        objects[toLoad.menuName].offset = offset;
+        objects[toLoad.menuName].forceExpire = 0;
+
+        if (toLoad.positionX) objects[toLoad.menuName].positionX = toLoad.positionX;
+        if (toLoad.positionY) objects[toLoad.menuName].positionY = toLoad.positionY;
+        if (toLoad.positionZ) objects[toLoad.menuName].positionZ = toLoad.positionZ;
+        if (toLoad.floating) objects[toLoad.menuName].floating = toLoad.floating;
+        if (toLoad.forces) objects[toLoad.menuName].forces = toLoad.forces;
     };
 
     let clicked = false;
@@ -337,11 +291,17 @@ export const mainInteraction = () => {
         cannonWorld.step(cannonStepTime, delta, 3);
 
         updatePhysics(scene, objects);
-        clicked && sequence.length > 0 && loadSequence(sequence);
+        clicked && sceneLoaded && sequence.length > 0 && loadSequence(sequence);
 
         ambientLight.intensity = parameters.ambientLight;
         renderer.toneMappingExposure = parameters.environmentLight;
         camera.position.y = parameters.camera;
+        if (objects.about) {
+            console.log(objects.about.body.mass);
+        }
+        if (objects.about) objects.about.body.mass = parameters.massAbout;
+        if (objects.contact) objects.contact.body.mass = parameters.massContact;
+        if (objects.work) objects.work.body.mass = parameters.massWork;
 
         cannonDebugger.update();
         renderer.render(scene, camera);
